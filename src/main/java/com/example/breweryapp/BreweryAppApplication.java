318547dbb97dc3b1aeb25cd48f6d89a638fa8638
Milan Andras Gyuricska <vapitea@gmail.com>
2020-08-23 17:14:41 +0200

    Create prject with Spring Initialzr

---
+++ src/main/java/com/example/breweryapp/BreweryAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.breweryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/breweryapp/BreweryAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.breweryapp;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BreweryAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
